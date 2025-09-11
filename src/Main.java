bf1fbabcfeb5d63afd35da0688cb857a79dd1696
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-12 00:04:28 +0530

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hello World! I am Saksham Pilane Nice to meet you all!!");
     }
 }
