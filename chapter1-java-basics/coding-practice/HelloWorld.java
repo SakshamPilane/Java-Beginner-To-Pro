c1748bd67d47eb34adab119ab35a50377a440e04
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-13 21:25:48 +0530

    updated birth-of-java file

---
+++ chapter1-java-basics/coding-practice/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World!\n");
+        System.out.print("My Name is Saksham Pilane.\n");
+        System.out.print("I am a B-Tech Computer Science Engineer here to help you guys with JAVA programming.");
+    }
+}
