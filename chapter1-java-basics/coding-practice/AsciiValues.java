a90778cb0014300bb5c14384956bcc4ea556cfe8
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-14 22:58:20 +0530

    Updated Java class Files in chapter1-java-basics\coding-practice added new class AsciiValues

---
+++ chapter1-java-basics/coding-practice/AsciiValues.java
@@ -0,0 +1,9 @@
+public class AsciiValues {
+    public static void main(String[] args)
+    {
+        // ASCII (American Standard Code for Information Interchange) Values are numeric coded values assigned to text characters
+        // example
+        char ch = 65; // This is a global ASCII value for capital A.
+        System.out.print(ch + " = ASCII value 65");
+    }
+}
