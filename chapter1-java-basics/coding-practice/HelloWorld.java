99fbea0e9feaf72552dbbb8b59abf88eb849fff7
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-14 23:10:57 +0530

    Updated Java class Files in chapter1-java-basics\coding-practice updated existing class HelloWorld.java

--- chapter1-java-basics/coding-practice/HelloWorld.java
+++ chapter1-java-basics/coding-practice/HelloWorld.java
@@ -1,8 +1,13 @@
-public class HelloWorld{
-    public static void main(String[] args)
-    {
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Saksham Pilane";
+        int age = 21;
+
         System.out.print("Hello World!\n");
-        System.out.print("My Name is Saksham Pilane.\n");
-        System.out.print("I am a B-Tech Computer Science Engineer here to help you guys with JAVA programming.");
+
+        // We are showcasing how to use variables inside print statements
+        // by concatenating them with strings using the + operator
+        System.out.println("My name is " + name + ", I am " + age + " years old.");
+        System.out.println("Currently I am a B-Tech Computer Science Engineer here to help you guys with JAVA programming.");
     }
 }
