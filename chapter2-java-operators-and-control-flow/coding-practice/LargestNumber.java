f967d22edae975cde8a23b58c61e3c602f257bb5
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-15 15:47:21 +0530

    added file LargestNumber.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/LargestNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class LargestNumber {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first Number: ");
+        float firstNumber = input.nextFloat();
+        System.out.print("Enter second Number: ");
+        float secondNumber = input.nextFloat();
+
+        if(firstNumber == secondNumber)
+        {
+            System.out.print("Largest Number = " + firstNumber + " and " + secondNumber + "\n");
+        }
+        else if (secondNumber > firstNumber)
+        {
+            System.out.print("Largest Number = " + secondNumber + "\n");
+        }
+        else
+        {
+            System.out.print("Largest Number = " + firstNumber + "\n");
+        }
+        input.close();
+    }
+}
