327143cbc13d98529c4aa41b2a1fcfb1dbe76a47
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-15 21:33:07 +0530

    added file LargestNumberAmongThree.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/LargestNumberAmongThree.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class LargestNumberAmongThree {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        float firstNumber = input.nextFloat();
+        System.out.print("Enter Second Number: ");
+        float secondNumber = input.nextFloat();
+        System.out.print("Enter Third Number: ");
+        float thirdNumber = input.nextFloat();
+
+        if (firstNumber == secondNumber && firstNumber == thirdNumber)
+        {
+            System.out.print("All Numbers are equal");
+        }
+        else if (firstNumber >= secondNumber && firstNumber >= thirdNumber)
+        {
+            System.out.print(firstNumber + " is the largest number");
+        }
+        else if (secondNumber >= thirdNumber)
+        {
+            System.out.print(secondNumber + " is the largest number");
+        }
+        else
+        {
+            System.out.print(thirdNumber + " is the largest number");
+        }
+        input.close();
+    }
+}
