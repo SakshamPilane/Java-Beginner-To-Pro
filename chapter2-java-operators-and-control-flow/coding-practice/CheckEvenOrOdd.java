0185869890c0ca8e01b087ca9540880e8db5ee89
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-15 21:37:39 +0530

    added file CheckEvenOrOdd.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/CheckEvenOrOdd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CheckEvenOrOdd {
+    public static void main(String[] args)
+    {
+        // can also be done using ternary operators
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0)
+        {
+            System.out.print(number + " is an Even number.");
+        }
+        else
+        {
+            System.out.print(number + " is an Odd number.");
+        }
+        input.close();
+    }
+}
