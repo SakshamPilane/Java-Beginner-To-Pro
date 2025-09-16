92a1cbc833ab3da8d3b3d59da682f07f78700912
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-16 21:09:57 +0530

    added a file FactorialOfNumbers.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/FactorialOfNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FactorialOfNumbers {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number to find its factorial: ");
+        int number = input.nextInt(), i = 1, factorial = 1;
+
+        while (i <= number)
+        {
+            factorial *= i;
+            i++;
+        }
+        System.out.print("Factorial of " + number + " = " + factorial);
+        input.close();
+    }
+}
