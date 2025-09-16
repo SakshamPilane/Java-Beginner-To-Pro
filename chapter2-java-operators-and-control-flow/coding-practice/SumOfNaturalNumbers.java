cb291cd8301e696085759aaa84f05afda173aefc
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-16 21:00:50 +0530

    added a file SumOfNaturalNumbers.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/SumOfNaturalNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumOfNaturalNumbers {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Range: ");
+        int range = input.nextInt(), i, sum = 0;
+        for (i=1;i<=range;i++)
+        {
+            sum += i;
+        }
+        System.out.print("Sum of Natural Numbers = " + sum);
+        input.close();
+    }
+}
