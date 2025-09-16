cb9e46b4040e298aa3a88ca86cb29261d6a4461c
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-16 21:16:48 +0530

    added a file MultiplicationTableCreation.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/MultiplicationTableCreation.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class MultiplicationTableCreation {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number to create its multiplication table: ");
+        int number = input.nextInt(), i;
+        for (i = 1; i <= 10; i++)
+        {
+            System.out.print(number + " x " + i + " = " + number * i + "\n");
+        }
+        input.close();
+    }
+}
