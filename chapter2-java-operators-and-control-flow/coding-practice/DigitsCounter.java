b05f6b09e567a4ba79276f9cf3bb591030edc408
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-17 19:16:35 +0530

    added a file DigitsCounter.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/DigitsCounter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class DigitsCounter {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number to find digit it contains: ");
+        int number = input.nextInt(), copyNumber = Math.abs(number), digitCount = 0;
+        if (number == 0)
+        {
+            digitCount = 1;
+        }
+        else {
+            while (copyNumber > 0) {
+                copyNumber /= 10;
+                digitCount++;
+            }
+        }
+        String digitWord = (digitCount > 1) ? " digits." : " digit.";
+        System.out.print(number + " has " + digitCount + digitWord);
+        input.close();
+    }
+}
