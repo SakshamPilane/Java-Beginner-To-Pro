b41f93efb44e729dc5fbec45f59d4d09a27bc4ec
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-17 23:02:36 +0530

    added files based on Pattern Building chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/Pattern_1_Square.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pattern_1_Square {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Size for drawing square pattern: ");
+        int n = input.nextInt(), i, j;
+        for ( i = 0; i < n; i++ )
+        {
+            for ( j = 0; j < n; j++ )
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        input.close();
+    }
+}
