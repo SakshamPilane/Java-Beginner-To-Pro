d6012f5219bd64eb6bfc968ee080b1e819cd5487
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-17 23:02:42 +0530

    added files based on Pattern Building chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/Pattern_2_HollowSquare.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pattern_2_HollowSquare {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Size for drawing hollow square pattern: ");
+        int n = input.nextInt(), i, j;
+        for ( i = 0; i < n; i++ )
+        {
+            for ( j = 0; j < n; j++ )
+            {
+                if (i==0 || i==n-1 || j==0 || j==n-1)
+                {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
+        input.close();
+    }
+}
