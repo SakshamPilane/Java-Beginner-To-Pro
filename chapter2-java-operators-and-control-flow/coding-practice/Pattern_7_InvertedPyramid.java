a2ae95053a6849b547d2cddb31a38586c544bf8a
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-19 23:54:10 +0530

    added a file Pattern_7_InvertedPyramid.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/Pattern_7_InvertedPyramid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Pattern_7_InvertedPyramid {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Size for drawing Pyramid pattern: ");
+        int n = input.nextInt(), i, j;
+        for ( i = 0; i < n; i++ )
+        {
+            int k = 0;
+            while (k < i)
+            {
+                System.out.print(" ");
+                k++;
+            }
+            for ( j = n-1; j >= i; j-- )
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        input.close();
+    }
+}
