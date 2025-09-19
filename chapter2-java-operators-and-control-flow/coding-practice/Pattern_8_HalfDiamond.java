092409e58e9179be9e8481b7a82f9e4e8851c362
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-20 00:07:01 +0530

    added a file Pattern_8_HalfDiamond.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/Pattern_8_HalfDiamond.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Pattern_8_HalfDiamond {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Size for drawing Half Diamond pattern: ");
+        int n = input.nextInt(), i, j;
+        for ( i = 0; i < n; i++ )
+        {
+            for ( j = 0; j <= i; j++ )
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        for ( i = 0; i < n; i++ )
+        {
+            for ( j = n-2; j >= i; j-- )
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        input.close();
+    }
+}
