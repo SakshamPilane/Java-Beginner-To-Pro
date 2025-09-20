d42e361f54be78fd3cbbb6dcb328225e2b00bd59
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-20 11:31:58 +0530

    added a file Pattern_11_HourGlass.java chapter2-java-operators-and-control-flow/coding-practice

---
+++ chapter2-java-operators-and-control-flow/coding-practice/Pattern_11_HourGlass.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Pattern_11_HourGlass {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Size for drawing Complete Hour Glass pattern: ");
+        int n = input.nextInt(), i, j;
+
+        for (i = 0; i < n; i++)
+        {
+            int k = 0;
+            while(k<i)
+            {
+                System.out.print(" ");
+                k++;
+            }
+            for (j = n-1; j >= i; j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+
+        for (i = 1; i < n; i++)
+        {
+            int k = n-1;
+            while(k>i)
+            {
+                System.out.print(" ");
+                k--;
+            }
+            for (j = 0; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        input.close();
+    }
+}
