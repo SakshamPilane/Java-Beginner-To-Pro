971df906d6b6878d1faa2dc4951a5da773049d87
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-21 10:23:40 +0530

    Adding a new file ReverseANumberMethod.java from chapter3-java-methods-and-functions/coding-practice

---
+++ chapter3-java-methods-and-functions/coding-practice/ReverseANumberMethod.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ReverseANumberMethod {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of terms to display sum of those digits: ");
+        int orgNumber = input.nextInt();
+        System.out.println("Reverse number = " + reverseNumber(orgNumber));
+        input.close();
+    }
+    public static int reverseNumber(int number)
+    {
+        int digit, revNumber = 0;
+        while(number > 0)
+        {
+            digit = number % 10;
+            revNumber = revNumber * 10 + digit;
+            number = number / 10;
+        }
+        return revNumber;
+    }
+}
