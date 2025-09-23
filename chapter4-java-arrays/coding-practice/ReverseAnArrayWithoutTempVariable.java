275e2808568beb782cbc1d539a3ff1f22058f0e8
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-09-23 13:01:29 +0530

    added a new file ReverseAnArrayWithoutTempVariable.java from chapter4-java-arrays/coding-practice

---
+++ chapter4-java-arrays/coding-practice/ReverseAnArrayWithoutTempVariable.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ReverseAnArrayWithoutTempVariable {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many Elements: ");
+        int i, n = input.nextInt();
+
+        int[] array = new int[n];
+        System.out.print("Enter Elements into array: ");
+        for (i = 0; i < n; i++)
+        {
+            array[i] = input.nextInt();
+        }
+
+        System.out.print("Array Entered: ");
+        for (i = 0; i < n; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        reverseArray(array);
+
+        input.close();
+    }
+
+    public static void reverseArray(int[] array)
+    {
+        int i, j;
+        for (i = 0, j = array.length - 1; i < array.length/2; i++, j--)
+        {
+            array[i] = array[i] + array[j];
+            array[j] = array[i] - array[j];
+            array[i] = array[i] - array[j];
+        }
+        System.out.print("Reversed Array: ");
+        for (i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
