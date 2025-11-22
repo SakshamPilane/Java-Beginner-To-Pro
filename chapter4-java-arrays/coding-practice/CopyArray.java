6f06a317be2f1a7182c08b770eb3da777d163225
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-11-23 04:58:18 +0530

    Added File CopyArray.java in chapter4-java-array/coding-practice

---
+++ chapter4-java-arrays/coding-practice/CopyArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CopyArray {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many Elements?: ");
+        int n = input.nextInt(), i, j = 0;
+
+        int[] array = new int[n];
+
+        System.out.print("Enter elements into array of size " + array.length + ": ");
+        for (i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        int[] copyArray = new int[array.length];
+
+        for (int element : array) {
+            copyArray[j++] = element;
+        }
+
+        System.out.print("New copy of array: ");
+        for (int element : copyArray) {
+            System.out.print(element + " ");
+        }
+
+        input.close();
+    }
+}
