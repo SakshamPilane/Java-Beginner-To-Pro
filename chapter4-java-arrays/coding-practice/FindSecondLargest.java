cb7ae8d1ad5238c75253648754e096aae25ba1ff
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-11-23 06:03:24 +0530

    Added File FindSecondLargest.java in chapter4-java-array/coding-practice

---
+++ chapter4-java-arrays/coding-practice/FindSecondLargest.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class FindSecondLargest {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many Elements?: ");
+        int n = input.nextInt(), i;
+
+        int[] array = new int[n];
+
+        System.out.print("Enter elements into array of size " + array.length + ": ");
+        for (i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        int number = penultimate(array);
+
+        System.out.print("Second highest number = " + number);
+
+        input.close();
+    }
+
+    public static int penultimate(int[] array)
+    {
+        if (array.length < 2) {
+            System.out.println("Need at least 2 elements!");
+        }
+
+        int highest = array[0], secondHighest = Integer.MIN_VALUE;
+
+        for (int num : array) {
+            if (highest < num)
+            {
+                secondHighest = highest;
+                highest = num;
+            }
+            else if (num > secondHighest && num != highest)
+            {
+                secondHighest = num;
+            }
+        }
+
+        if (secondHighest == Integer.MIN_VALUE)
+        {
+            System.out.println("No second largest element exists!");
+        }
+        return secondHighest;
+    }
+}
