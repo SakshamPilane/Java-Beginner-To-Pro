c0571e80308929675b370441d44d9333d1dcfd54
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-11-23 06:17:49 +0530

    Added File ArrayReverse.java in chapter4-java-array/coding-practice

---
+++ chapter4-java-arrays/coding-practice/ArrayReverse.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ArrayReverse {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many Elements?: ");
+        int n = input.nextInt(), i;
+
+        int[] array = new int[n];
+
+        System.out.print("Enter elements into array of size " + array.length + ": ");
+        for (i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        System.out.print("Array: ");
+        for (int num : array)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        revArray(array);
+
+        input.close();
+    }
+
+    public static void revArray(int[] array)
+    {
+        int n = array.length, i , j, temp = 0;
+        for (i = 0, j = n - 1; i < n / 2; i++, j--) {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        System.out.print("Reversed Array: ");
+        for (int num : array)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
