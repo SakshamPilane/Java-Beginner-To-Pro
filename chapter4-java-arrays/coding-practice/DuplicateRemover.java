e1d9266c99afbf99feb9c1fbaf8e09f4f4aa5ee0
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-11-24 13:59:52 +0530

    Added File DuplicateRemover.java in chapter4-java-array/coding-practice

---
+++ chapter4-java-arrays/coding-practice/DuplicateRemover.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DuplicateRemover {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many Elements?: ");
+        int n = input.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.print("Enter elements into array of size " + array.length + ": ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        Arrays.sort(array);
+        int size = removeDuplicates(array);
+
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        input.close();
+    }
+
+    public static int removeDuplicates(int[] arr) {
+        int j = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+        return j + 1;
+    }
+}
