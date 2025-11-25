81a67f83361569135b8a751b62f2e596ed1421cc
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-11-25 19:50:30 +0530

    Added File BubbleSort.java in chapter4-java-array/coding-practice

---
+++ chapter4-java-arrays/coding-practice/BubbleSort.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter size for array: ");
+        int n = input.nextInt();
+
+        if(n <= 0)
+        {
+            System.out.println("Array size cannot be less than or equal to 0! EXITING.....");
+            return;
+        }
+
+        int[] array = new int[n];
+
+        System.out.print("Enter Elements into array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = input.nextInt();
+        }
+
+        System.out.print("Array Entered: ");
+        for (int num : array)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        bSort(array);
+
+        System.out.print("Sorted Array: ");
+        for (int num : array)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        input.close();
+    }
+
+    public static void bSort(int[] array)
+    {
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j+1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+            {
+                System.out.println("Already Sorted!!");
+                break;
+            }
+        }
+    }
+}
