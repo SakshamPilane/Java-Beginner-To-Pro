6461f8ba8b8734fa8c5f6760307ba96a5e3096bc
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-11-26 12:19:59 +0530

    Added File RepeatingElements.java in chapter4-java-array/coding-practice

---
+++ chapter4-java-arrays/coding-practice/RepeatingElements.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RepeatingElements {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter size for array: ");
+        int n = input.nextInt();
+
+        if(n <= 0)
+        {
+            System.out.println("Array size cannot be less than or equal to 0! EXITING.....");
+            return;
+        }
+
+        int[] array = new int[n];
+
+        System.out.print("Enter Elements into array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = input.nextInt();
+        }
+
+        System.out.println("Array Entered: " + Arrays.toString(array));
+
+        Arrays.sort(array);
+
+        find(array);
+    }
+
+    public static void find(int[] arr)
+    {
+        int firstRepeating = -1, firstNonRepeating = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    firstRepeating = arr[i];
+                    i = arr.length;
+                    break;
+                }
+            }
+        }
+
+        for (int k : arr) {
+
+            int count = 0;
+            for (int i : arr) {
+                if (k == i) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                firstNonRepeating = k;
+                break;
+            }
+        }
+
+        System.out.println("First Repeating = " + firstRepeating);
+        System.out.println("First Non-Repeating = " + firstNonRepeating);
+    }
+}
