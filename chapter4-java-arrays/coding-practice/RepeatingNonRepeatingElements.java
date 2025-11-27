7daef60c0675f19a5de318a72d7ed6cfcfe408e7
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-11-27 09:39:54 +0530

    Updated File RepeatingNonRepeatingElements.java in chapter4-java-array/coding-practice

--- chapter4-java-arrays/coding-practice/RepeatingNonRepeatingElements.java
+++ chapter4-java-arrays/coding-practice/RepeatingNonRepeatingElements.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class RepeatingElements {
+public class RepeatingNonRepeatingElements {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
