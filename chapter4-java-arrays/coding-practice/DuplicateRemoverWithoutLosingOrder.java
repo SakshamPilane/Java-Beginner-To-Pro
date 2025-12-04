c6d6cb8874c752264d53991c0a685c614b417dd1
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-12-04 23:10:33 +0530

    Added File DuplicateRemoverWithoutLosingOrder.java in chapter4-java-array/coding-practice

---
+++ chapter4-java-arrays/coding-practice/DuplicateRemoverWithoutLosingOrder.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class DuplicateRemoverWithoutLosingOrder {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many Elements?: ");
+        int n = input.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.print("Enter elements into array of size " + array.length + ": ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        int size = removeDuplicates(array);
+
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        input.close();
+    }
+
+    public static int removeDuplicates(int[] array) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            boolean flagFound = false;
+            for (int j = 0; j < index; j++) {
+                if (array[i] == array[j])
+                {
+                    flagFound = true;
+                    break;
+                }
+            }
+            if (!flagFound)
+            {
+                array[index] = array[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
