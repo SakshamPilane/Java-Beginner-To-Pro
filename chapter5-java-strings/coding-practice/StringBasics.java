3d17293dc9f8ff74b58e9a517fd2ff4542becd22
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-12-06 21:22:48 +0530

    Updated File StringBasics.java in chapter5-java-strings/coding-practice

--- chapter5-java-strings/coding-practice/StringBasics.java
+++ chapter5-java-strings/coding-practice/StringBasics.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class StringBasics {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("User enter your name: ");
@@ -10,19 +9,35 @@ public class StringBasics {
 
         System.out.println("Name entered = " + str);
 
-        char[] charArray = str.toCharArray();
-        System.out.println("Total characters in string are = " + countChar(charArray));
-
+        System.out.println("Total characters in string are = " + countChar(str));
+        countVowelsAndConsonants(str);
         input.close();
     }
 
-    public static int countChar(char[] charArray)
-    {
+    public static int countChar(String str) {
+        char[] charArray = str.toCharArray();
         int count = 0;
-        for (char _ : charArray)
-        {
+        for (char _ : charArray) {
             count++;
         }
         return count;
+    }
+
+    public static void countVowelsAndConsonants(String str) {
+        char[] charArray = str.toLowerCase().toCharArray();
+        int vowels = 0, consonants = 0;
+        for (int ch : charArray) {
+            if ((ch >= 97 && ch <= 122))
+            {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                }
+                else
+                {
+                    consonants++;
+                }
+            }
+        }
+        System.out.println("Total vowels are = " + vowels + " and consonants are = " + consonants);
     }
 }
