9e0b145a286b652f05169fd44edc82b42612c28f
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-12-07 22:55:32 +0530

    Added File StringReverse.java in chapter5-java-strings/coding-practice

---
+++ chapter5-java-strings/coding-practice/StringReverse.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string to reverse it: ");
+        String str = input.nextLine();
+
+        // get user input
+        System.out.println("String entered = " + str);
+
+        revString(str);
+
+        input.close();
+    }
+
+    public static void revString(String str)
+    {
+        char[] charArray = str.toCharArray();
+
+        for (int i = 0, j = charArray.length - 1; i < charArray.length / 2; i++, j--) {
+            char c = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = c;
+        }
+
+        System.out.print("Reverse of given String = ");
+        for (char c : charArray)
+        {
+            System.out.print(c);
+        }
+    }
+}
