a218d4781f8a21bb8d81cba456bff7febcf2bee2
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-12-07 16:18:38 +0530

    Updated File StringBasics.java in chapter5-java-strings/coding-practice

--- chapter5-java-strings/coding-practice/StringBasics.java
+++ chapter5-java-strings/coding-practice/StringBasics.java
@@ -7,13 +7,54 @@ public class StringBasics {
         System.out.print("User enter your name: ");
         String str = input.nextLine();
 
+        // get user input
         System.out.println("Name entered = " + str);
 
+        // convert to lowercase and uppercase
+        convertToLowerCase(str);
+        convertToUpperCase(str);
+
+        // count total characters in array
         System.out.println("Total characters in string are = " + countChar(str));
         countVowelsAndConsonants(str);
+
+        // check if palindrome
         String ifPalindrome = (checkPalindrome(str)) ? " is a Palindrome." : " is a not Palindrome.";
         System.out.println("String = " + str + ifPalindrome);
+
         input.close();
+    }
+
+    public static void convertToLowerCase(String str) {
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            if (c >= 'A' && c <= 'Z') {
+                charArray[i] = (char)(c + 32);
+            }
+        }
+
+        System.out.print("Lowe case String = ");
+        for (char c : charArray) {
+            System.out.print(c);
+        }
+        System.out.println();
+    }
+
+    public static void convertToUpperCase(String str) {
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            if (c >= 'a' && c <= 'z') {
+                charArray[i] = (char)(c - 32);
+            }
+        }
+
+        System.out.print("Upper case String = ");
+        for (char c : charArray) {
+            System.out.print(c);
+        }
+        System.out.println();
     }
 
     public static int countChar(String str) {
