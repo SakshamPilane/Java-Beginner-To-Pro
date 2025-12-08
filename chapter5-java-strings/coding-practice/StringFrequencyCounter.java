782585901c1621c954d7b13ef153aacf3519569d
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-12-08 06:07:39 +0530

    Updated File StringFrequencyCounter.java in chapter5-java-strings/coding-practice

--- chapter5-java-strings/coding-practice/StringFrequencyCounter.java
+++ chapter5-java-strings/coding-practice/StringFrequencyCounter.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StringFrequencyCounter {
@@ -17,5 +18,20 @@ public class StringFrequencyCounter {
 
     public static void freqCount(String str)
     {
+        char[] charArray = str.toLowerCase().toCharArray();
+        Arrays.sort(charArray);
+        int count = 1;
+        for (int i = 1; i < charArray.length; i++) {
+            if (charArray[i] == charArray[i-1])
+            {
+                count++;
+            }
+            else
+            {
+                System.out.println(charArray[i - 1] + " occurs " + count + " time(s)");
+                count = 1;
+            }
+        }
+        System.out.println(charArray[charArray.length - 1] + " occurs " + count + " time(s)");
     }
 }
