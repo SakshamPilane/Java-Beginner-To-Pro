460e0bd49273b2683c0daeb74e6db7984a3f4173
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-12-09 14:18:52 +0530

    Added File VowelsRemover.java in chapter5-java-strings/coding-practice

---
+++ chapter5-java-strings/coding-practice/VowelsRemover.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class VowelsRemover {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = input.nextLine();
+
+        if (str.isEmpty()) return;
+
+        System.out.println("String entered = " + str);
+
+        str = removeVowels(str);
+
+        System.out.println("New String without vowels = " + str);
+
+        input.close();
+    }
+
+    public static String removeVowels(String str)
+    {
+        char[] charArray = str.toCharArray();
+        int j = 0;
+        for (char c : charArray) {
+            if (!isVowel(c))
+            {
+                charArray[j++] = c;
+            }
+        }
+        return new String(charArray, 0, j);
+    }
+
+    public static boolean isVowel(char c)
+    {
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+}
