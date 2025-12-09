b29d27949b3e6556c80d4e5e8f14f0085bacc8ae
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2025-12-09 18:05:51 +0530

    Added File StringCopy.java in chapter5-java-strings/coding-practice

---
+++ chapter5-java-strings/coding-practice/StringCopy.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class StringCopy {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a String: ");
+        String str = input.nextLine();
+
+        if (str.isEmpty())
+        {
+            System.out.print("No String Entered! Exiting.....");
+            return;
+        }
+
+        System.out.println("String Entered = " + str);
+
+        String newStr = strCopy(str);
+
+        System.out.print("New String = " + newStr);
+
+        input.close();
+    }
+
+    public static String strCopy(String str)
+    {
+        char[] charArray = str.toCharArray();
+        char[] newCharArray = new char[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+            newCharArray[i] = charArray[i];
+        }
+        // or use of System.arraycopy(charArray, 0, newCharArray, 0, charArray.length);
+        return new String(newCharArray);
+    }
+}
