5f28c57c99bb8b49ca738275ad9793103f19c18e
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-01-02 15:03:34 +0530

    Updated File UserDefinedFunctions.java in chapter5-java-strings/coding-practice

--- chapter5-java-strings/coding-practice/UserDefinedFunctions.java
+++ chapter5-java-strings/coding-practice/UserDefinedFunctions.java
@@ -7,29 +7,73 @@ public class UserDefinedFunctions {
 
         System.out.println("Enter main String: ");
         String mainStr = input.nextLine();
-
-        System.out.println("Enter sub string: ");
-        String subStr = input.nextLine();
-
         if (mainStr.isEmpty())
         {
-            System.out.println("String is Empty....Exiting....!");
+            System.out.print("String is Empty....Exiting....!");
             input.close();
             return;
         }
 
-        int position = indexOf(mainStr,subStr);
+        System.out.print("Enter sub string: ");
+        String subStr = input.nextLine();
 
-        if (position == -1)
+        while (true)
         {
-            System.out.println("String \"" + subStr + "\" not found in \"" + mainStr + "\".");
-        }
-        else
-        {
-            System.out.println("String \"" + subStr + "\" found at location = " + position + " in \"" + mainStr + "\".");
-        }
+            System.out.println("***************************************");
+            System.out.println("1. Find Index of a sub string.");
+            System.out.println("2. Find Starting Index of a sub string.");
+            System.out.println("3. Find Ending Index of a sub string.");
+            System.out.println("4. Exit!");
+            System.out.println("***************************************");
+            System.out.println("Enter a choice: ");
+            int choice = input.nextInt();
+            input.nextLine();
+            switch (choice)
+            {
+                case 1:
+                    int position1 = indexOf(mainStr,subStr);
 
-        input.close();
+                    if (position1 == -1)
+                    {
+                        System.out.println("String \"" + subStr + "\" not found in \"" + mainStr + "\".");
+                    }
+                    else
+                    {
+                        System.out.println("String \"" + subStr + "\" found at location = " + position1 + " in \"" + mainStr + "\".");
+                    }
+                    break;
+
+                case 2:
+                    if (!startsWith(mainStr,subStr))
+                    {
+                        System.out.println("String \"" + mainStr + "\" does not start with \"" + subStr + "\".");
+                    }
+                    else
+                    {
+                        System.out.println("String \"" + mainStr + "\" starts with \"" + subStr + "\".");
+                    }
+                    break;
+
+                case 3:
+                    if (!endsWith(mainStr,subStr))
+                    {
+                        System.out.println("String \"" + mainStr + "\" does not end with \"" + subStr + "\".");
+                    }
+                    else
+                    {
+                        System.out.println("String \"" + mainStr + "\" ends with \"" + subStr + "\".");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Exiting.....");
+                    input.close();
+                    return;
+
+                default:
+                    System.out.println("Wrong Choice Try Again!!");
+            }
+        }
     }
 
     public static int indexOf(String mainStr, String subStr)
@@ -52,5 +96,43 @@ public class UserDefinedFunctions {
             }
         }
         return -1;
+    }
+
+    public static boolean startsWith(String mainStr, String subStr)
+    {
+        if (subStr.isEmpty())
+        {
+            return true;
+        }
+        if (subStr.length() > mainStr.length())
+        {
+            return false;
+        }
+        for (int i = 0; i < subStr.length(); i++) {
+            if (mainStr.charAt(i) != subStr.charAt(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean endsWith(String mainStr, String subStr)
+    {
+        if (subStr.isEmpty())
+        {
+            return true;
+        }
+        if (subStr.length() > mainStr.length())
+        {
+            return false;
+        }
+        for (int i = mainStr.length() - subStr.length(), j = 0; i < mainStr.length(); i++, j++) {
+            if (mainStr.charAt(i) != subStr.charAt(j))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
