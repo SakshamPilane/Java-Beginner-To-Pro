5aae15e4803969e7447fc856983a37b2b491ac3c
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-01-15 23:33:34 +0530

    added new packages basic_17 solving getter and setter methods

---
+++ chapter6-java-oop/coding-practice-basics/basic_17/Main.java
@@ -0,0 +1,11 @@
+package basic_17;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Saksham");
+        s.setRollNo(10);
+        System.out.println("Name = " + s.getName());
+        System.out.println("Roll No = " + s.getRollNo());
+    }
+}
---
+++ chapter6-java-oop/coding-practice-basics/basic_17/Student.java
@@ -0,0 +1,26 @@
+package basic_17;
+
+public class Student {
+    private int rollNo;
+    private String name;
+
+    public int getRollNo()
+    {
+        return this.rollNo;
+    }
+
+    public void setRollNo(int rollNo)
+    {
+        this.rollNo = rollNo;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
