fe6fd798260a9c6dfb2f99ec394b42e1de8f739e
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-02-01 09:38:07 +0530

    Exception Handling in main chapter7-java-exception-handling/coding-practice/coding_10

---
+++ chapter7-java-exception-handling/coding-practice/coding_11/Test.java
@@ -0,0 +1,27 @@
+package coding_11;
+
+public class Test {
+    public static double divide(double a, double b) throws ArithmeticException
+    {
+        if (b == 0)
+        {
+            throw new ArithmeticException("Division By Zero is Invalid!");
+        }
+        return a/b; // JVM throws ArithmeticException automatically even if throws statement is not written!
+    }
+
+    public static void main(String[] args) {
+        double a = 10, b = 0;
+        try {
+            double result = divide(a,b);
+            System.out.println(a + " / " + b + " = " + result);
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("Program Ends!");
+        }
+    }
+}
