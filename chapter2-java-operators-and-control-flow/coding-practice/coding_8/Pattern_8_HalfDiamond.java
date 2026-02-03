c6aeeb0ca2569dcd9735f83fcded309111beb0df
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-02-03 17:30:58 +0530

    updates

--- chapter2-java-operators-and-control-flow/coding-practice/coding_8/Pattern_8_HalfDiamond.java
+++ chapter2-java-operators-and-control-flow/coding-practice/coding_8/Pattern_8_HalfDiamond.java
@@ -6,23 +6,61 @@ public class Pattern_8_HalfDiamond {
     {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Size for drawing Half Diamond pattern: ");
-        int n = input.nextInt(), i, j;
-        for ( i = 0; i < n; i++ )
-        {
-            for ( j = 0; j <= i; j++ )
-            {
-                System.out.print("* ");
-            }
-            System.out.print("\n");
-        }
-        for ( i = 0; i < n; i++ )
-        {
-            for ( j = n-2; j >= i; j-- )
-            {
-                System.out.print("* ");
-            }
-            System.out.print("\n");
-        }
+        int n = input.nextInt();
+        rightHalf(n);
+        leftHalf(n);
         input.close();
+    }
+
+    public static void rightHalf(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = n - 2; j >= i; j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void leftHalf(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int k = n-1;
+            while (k > i)
+            {
+                System.out.print("  ");
+                k--;
+            }
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++)
+        {
+            int k = 0;
+            while (k <= i)
+            {
+                System.out.print("  ");
+                k++;
+            }
+            for (int j = n-2; j >= i; j--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
