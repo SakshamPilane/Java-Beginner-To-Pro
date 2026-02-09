6501275a213c0ea044e4e613083e2b15ac35ba19
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-02-09 15:18:39 +0530

    updates

--- chapter4-java-arrays/coding-practice/coding_20/FindPairsWhoseSumIs.java
+++ chapter4-java-arrays/coding-practice/coding_20/FindPairsWhoseSumIs.java
@@ -1,7 +1,9 @@
+package coding_20;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class FindPairsWhoseSumIs {
+class FindPairsWhoseSumIs {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
