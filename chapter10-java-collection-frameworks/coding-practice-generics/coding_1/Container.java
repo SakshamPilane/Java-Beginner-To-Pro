e716ec61fe89969c76b634015a8df5e3ae04687d
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-02-13 15:27:48 +0530

    updates

---
+++ chapter10-java-collection-frameworks/coding-practice-generics/coding_1/Container.java
@@ -0,0 +1,16 @@
+package coding_1;
+
+public class Container<T> {
+
+    private T value;
+
+    public void setValue(T value)
+    {
+        this.value = value;
+    }
+
+    public T getValue()
+    {
+        return value;
+    }
+}
---
+++ chapter10-java-collection-frameworks/coding-practice-generics/coding_1/Test.java
@@ -0,0 +1,17 @@
+package coding_1;
+
+public class Test {
+    public static void main(String[] args) {
+        Container<String> c1 = new Container<>();
+        c1.setValue("Saksham");
+        System.out.println(c1.getValue());
+
+        Container<Integer> c2 = new Container<>();
+        c2.setValue(21);
+        System.out.println(c2.getValue());
+
+        Container<Double> c3 = new Container<>();
+        c3.setValue(100.33);
+        System.out.println(c3.getValue());
+    }
+}
