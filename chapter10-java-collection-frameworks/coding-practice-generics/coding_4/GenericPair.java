8412d958ac4ab70fd9d1789e6cd55329e876f5ec
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-02-14 19:16:44 +0530

    Generic Pair Class Example in Chapter-10-java-collection-frameworks/coding-practice-generics/coding_4 package

---
+++ chapter10-java-collection-frameworks/coding-practice-generics/coding_4/GenericPair.java
@@ -0,0 +1,25 @@
+package coding_4;
+
+public class GenericPair <T, V>{
+    private T value1;
+    private V value2;
+
+    public T getValue1()
+    {
+        return value1;
+    }
+
+    public V getValue2() {
+        return value2;
+    }
+
+    public void setValue1(T value1)
+    {
+        this.value1 = value1;
+    }
+
+    public void setValue2(V value2)
+    {
+        this.value2 = value2;
+    }
+}
---
+++ chapter10-java-collection-frameworks/coding-practice-generics/coding_4/Test.java
@@ -0,0 +1,20 @@
+package coding_4;
+
+public class Test {
+
+    public static void main(String[] args) {
+        GenericPair<Integer, String> gp = new GenericPair<>();
+        gp.setValue1(21);
+        gp.setValue2("Saksham");
+
+        System.out.println("Age = " + gp.getValue1());
+        System.out.println("Name = " + gp.getValue2());
+
+        GenericPair<String, Double> gpNewObj = new GenericPair<>();
+        gpNewObj.setValue1("Saksham Pilane");
+        gpNewObj.setValue2(21.11);
+
+        System.out.println("Exact Name = " + gpNewObj.getValue1());
+        System.out.println("Exact Age = " + gpNewObj.getValue2());
+    }
+}
