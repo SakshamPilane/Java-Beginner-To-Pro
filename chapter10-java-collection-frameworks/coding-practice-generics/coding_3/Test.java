8797fea60b44bba0a329aa61459ff0c82fcc9801
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-02-14 19:00:14 +0530

    Generic Static method Swapping Elements Example in Chapter-10-java-collection-frameworks/coding-practice-generics/coding_3 package

---
+++ chapter10-java-collection-frameworks/coding-practice-generics/coding_3/Test.java
@@ -0,0 +1,42 @@
+package coding_3;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter 2 Numbers: ");
+        Integer[] swapArrays1 = new Integer[2];
+        swapArrays1[0] = input.nextInt();
+        swapArrays1[1] = input.nextInt();
+        swap(swapArrays1,0,1);
+
+        System.out.print("Enter 2 Strings: ");
+        String[] swapArrays2 = new String[2];
+        swapArrays2[0] = input.next();
+        swapArrays2[1] = input.next();
+        swap(swapArrays2,0,1);
+
+        for (int i = 0; i < 2; i++) {
+            System.out.print(swapArrays1[i] + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < 2; i++) {
+            System.out.print(swapArrays2[i] + " ");
+        }
+
+        input.close();
+    }
+
+    public static <T> void swap(T[] array, int i, int j)
+    {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+}
