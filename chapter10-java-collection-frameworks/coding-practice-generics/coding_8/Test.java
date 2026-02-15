eaf500932233256983d2b9ee936907fcbe2ff4d9
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-02-15 20:00:57 +0530

    Generic Upper Bounded Class using Comparable to find max of all elements in Chapter-10-java-collection-frameworks/coding-practice-generics/coding_8 package

---
+++ chapter10-java-collection-frameworks/coding-practice-generics/coding_8/Test.java
@@ -0,0 +1,34 @@
+package coding_8;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter size of an array: ");
+        int size = input.nextInt();
+
+        Integer[] array = new Integer[size];
+
+        System.out.print("Enter elements into array: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        System.out.println("Maximum Found of all Elements from array = " + findMax(array));
+
+        input.close();
+    }
+
+    public static <T extends Comparable<T>> T findMax(T[] array)
+    {
+        T maxNumber = array[0];
+        for (T t : array) {
+            if (t.compareTo(maxNumber) > 0) {
+                maxNumber = t;
+            }
+        }
+        return maxNumber;
+    }
+}
