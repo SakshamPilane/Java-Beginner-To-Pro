73e9f31bed6f8eef51f63c8af9b77c746b3ab06d
SakshamMarutiPilane <Sakshampilane.04@gmail.com>
2026-02-16 12:17:45 +0530

    Wildcard generic method uses list to Print Values Passed to it Chapter-10-java-collection-frameworks/coding-practice-generics/coding_11 package

---
+++ chapter10-java-collection-frameworks/coding-practice-generics/coding_11/Main.java
@@ -0,0 +1,24 @@
+package coding_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Double> list = new ArrayList<>();
+        list.add(12.3);
+        list.add(13.55);
+        list.add(14.23);
+        list.add(15D);
+        list.add(16D);
+        printNumber(list);
+    }
+
+    public static void printNumber(final List<? extends Number> list)
+    {
+        for (Number num : list)
+        {
+            System.out.print(num + " ");
+        }
+    }
+}
